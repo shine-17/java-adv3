83bbe1e641cc7266d18c4ff62c3b3978b3856eb3
dohun <shine_17@naver.com>
2025-10-17 22:36:47 +0900

    add section3: lambda & Functional Interface

---
+++ src/lambda/ex1/M5Return.java
@@ -0,0 +1,30 @@
+package lambda.ex1;
+
+import lambda.MyFunction;
+
+public class M5Return {
+    public static void main(String[] args) {
+
+        MyFunction add = getOperation("add");
+        MyFunction sub = getOperation("sub");
+        MyFunction xxx = getOperation("xxx");
+
+        System.out.println("add.apply(1,2) = " + add.apply(1,2));
+        System.out.println("sub.apply(1,2) = " + sub.apply(1,2));
+        System.out.println("xxx.apply(1,2) = " + xxx.apply(1,2));
+    }
+
+    private static MyFunction getOperation(String operator) {
+        switch(operator) {
+            case "add" -> {
+                return (a, b) -> a + b;
+            }
+            case "sub" -> {
+                return (a, b) -> a- b;
+            }
+            default -> {
+                return (a, b) -> 0;
+            }
+        }
+    }
+}
---
+++ src/lambda/ex2/BuildGreeterExample.java
@@ -0,0 +1,17 @@
+package lambda.ex2;
+
+public class BuildGreeterExample {
+    public static void main(String[] args) {
+        StringFunction helloGreeter = buildGreeter("Hello");
+        StringFunction hiGreeter = buildGreeter("Hi");
+
+        System.out.println(helloGreeter.apply("Java"));
+        System.out.println(hiGreeter.apply("Lambda"));
+    }
+
+
+    // 고차 함수, greeting 문자열을 받아, "새로운 함수를" 반환
+    public static StringFunction buildGreeter(String greeting) {
+        return name -> greeting + ", " + name;
+    }
+}

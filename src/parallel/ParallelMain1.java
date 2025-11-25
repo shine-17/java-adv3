6b2dcaca866c81a2ab3574ca21c994808111b894
dohun <shine_17@naver.com>
2025-11-25 21:35:20 +0900

    feat: add section 13 - parallel (fork/join)

---
+++ src/parallel/ParallelMain1.java
@@ -0,0 +1,20 @@
+package parallel;
+
+import util.MyLogger;
+
+import java.util.stream.IntStream;
+
+import static util.MyLogger.*;
+
+public class ParallelMain1 {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        int sum = IntStream.rangeClosed(1, 8)
+                .map(HeavyJob::heavyTask)
+                .reduce(0, Integer::sum);
+
+        long endTime = System.currentTimeMillis();
+        log("time: " + (endTime - startTime) + "ms, sum: " + sum);
+    }
+}
